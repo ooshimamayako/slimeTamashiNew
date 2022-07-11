728f0916ea7608d8611f6abd7bcef2f7d6aa6eae
DESKTOP-MFU643B\maya1 <maya13na11@gmail.com>
2022-07-11 20:52:43 +0900

    一旦コミット

---
+++ Assets/GameTimer.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameTimer : MonoBehaviour
+{
+
+    private float time = 3.0f;
+    public Text timerText;
+    public bool isTimeUp;
+
+    void Start()
+    {
+        isTimeUp = false;
+    }
+
+    void Update()
+    {
+        if (0 < time)
+        {
+            time -= Time.deltaTime;
+            timerText.text = time.ToString("F1");
+        }
+        else if (time < 0)
+        {
+            isTimeUp = true;
+            SceneManager.LoadScene("result");
+        }
+    }
+}
+}
